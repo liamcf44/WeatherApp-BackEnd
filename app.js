3f2be3373ab64b5abcaa66224e5e4ee5130aee35
liamcf44 <liamcf44@protonmail.com>
2018-06-08 12:13:58 +0100

    next commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { checkCityName } = require('./controller');
 
-const DB_URL = require('./config');
+const DB_URL = require('./config/index');
 
 mongoose
   .connect(DB_URL)
