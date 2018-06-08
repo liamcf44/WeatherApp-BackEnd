9e6147ef962efd223fcc918f3b8222d618145606
liamcf44 <liamcf44@protonmail.com>
2018-06-08 12:33:15 +0100

    set up on heroku

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use((err, req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+  console.log(err);
   res.status(500).send({ message: 'Internal Server Error' });
 });
 
--- controller.js
+++ controller.js
@@ -1,11 +1,11 @@
-const { TownSchema } = require('./model');
+const TownSchema = require('./model');
 
 exports.checkCityName = (req, res, next) => {
   const { cityname } = req.params;
   return TownSchema.find({ name: cityname })
     .then(result => {
       console.log(result);
-      res.status(200).second({ result });
+      res.status(200).send({ result });
     })
     .catch(err => {
       console.log(err);
