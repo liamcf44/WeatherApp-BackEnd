b867bafa950930391bdc31f69abb5cbde9c3be8f
liamcf44 <liamcf44@protonmail.com>
2018-06-08 12:03:56 +0100

    Commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { checkCityName } = require('controller.js');
 
 const DB_URL = require('./config');
 
@@ -9,5 +11,25 @@ mongoose
     console.log(`Connected to Database at ${DB_URL}`);
   })
   .catch(console.log);
+
+app.use(bodyParser.json());
+
+app.get('/:cityname', checkCityName);
+
+app.get('/*', (req, res, next) => {
+  next({ status: 404 });
+});
+
+app.use((err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({ message: '404: Page not found' });
+  } else if (err.status === 400) {
+    res.status(400).send({ message: '400: Bad Request' });
+  } else next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: 'Internal Server Error' });
+});
 
 module.exports = app;
---
+++ controller.js
@@ -0,0 +1,14 @@
+const { TownSchema } = require('./model');
+
+exports.checkCityName = (req, res, next) => {
+  const { cityname } = req.params;
+  return TownSchema.find({ name: cityname })
+    .then(result => {
+      console.log(result);
+      res.status(200).second({ result });
+    })
+    .catch(err => {
+      console.log(err);
+      next({ status: 400 });
+    });
+};
